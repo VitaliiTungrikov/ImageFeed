ae8a3b9eb881743bec87882145a1b49fb721f808
Vitalii <vtungrikov@yandex.ru>
2024-02-22 17:45:31 +0300

    Комит

---
+++ SingleImageViewController/SingleImageViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by admin on 22.02.2024.
+//
+
+import Foundation
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+}
