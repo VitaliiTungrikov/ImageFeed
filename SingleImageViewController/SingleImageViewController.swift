acd1efdef8f13ac662e64913d18f93bd075d025a
Vitalii <vtungrikov@yandex.ru>
2024-02-24 16:09:07 +0300

    Комит

--- SingleImageViewController/SingleImageViewController.swift
+++ SingleImageViewController/SingleImageViewController.swift
@@ -9,5 +9,21 @@ import Foundation
 import UIKit
 
 final class SingleImageViewController: UIViewController {
+
     @IBOutlet var imageView: UIImageView!
+
+    var image: UIImage?{
+        didSet {
+            guard isViewLoaded else { return }
+            imageView.image = image
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imageView.image = image
+    }
+    @IBAction func didTapBackButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
 }
